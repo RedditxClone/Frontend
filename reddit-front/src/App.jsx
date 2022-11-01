b9c04375483fc2a8e3d09e1efa107fc061fe1077
ahmedasad236 <ahmedasad236@gmail.com>
2022-11-01 16:28:44 +0200

    feature/18/create_subreddit_ui
    - Change the directory name 'createCommunity' to 'CreateCommunity' as the folders'names should be pascal case

--- reddit-front/src/App.jsx
+++ reddit-front/src/App.jsx
@@ -1,7 +1,8 @@
 import './index.css';
+import CreateCommunity from './components/CreateCommunity/CreateCommunity';
 
 function App() {
-  return <>App</>;
+  return <CreateCommunity />;
 }
 
 export default App;
