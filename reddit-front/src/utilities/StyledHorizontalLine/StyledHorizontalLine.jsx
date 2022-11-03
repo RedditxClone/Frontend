67dc3fbe46f96db4bae84874fc4793791d89f5a2
MUSTAFA-HAMZAWY <hamzawy2019579@gmail.com>
2022-11-03 20:49:36 +0200

    Added some utilities that will be used in many components
    feature/37/create_subreddit_cards

---
+++ reddit-front/src/utilities/StyledHorizontalLine/StyledHorizontalLine.jsx
@@ -0,0 +1,12 @@
+export default function StyledHorizontalLine() {
+  return (
+    <hr
+      style={{
+        backgroundColor: '#1a1a1b12',
+        border: 'none',
+        height: '1px',
+        margin: '16px 0'
+      }}
+    />
+  );
+}
