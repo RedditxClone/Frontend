515cfeaa0af96fa0d4f866cfa98fcacb547b8db1
ahmedasad236 <ahmedasad236@gmail.com>
2022-11-04 17:37:18 +0200

    feature/38/authentication_for_login_sign_up
    - Create slice for authentication
    - Create Async Thunk functions for login & signup & forget password & forget username

--- reddit-front/src/store/slices/AuthSlice.jsx
+++ reddit-front/src/store/slices/AuthSlice.jsx
@@ -1,16 +1,15 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const INITIAL_STATE = {
-  user: '',
-  token: '',
-  msg: '',
+  user: {},
+  isAuth: false,
+  isLoading: false,
   error: '',
-  isLoggedIn: false,
-  isLoading: false
+  mess: ''
 };
 
-export const signUpUser = createAsyncThunk('signup', async (body) => {
-  const res = await fetch('', {
+export const signUp = createAsyncThunk('user/signup', async (body) => {
+  const res = await fetch('/auth/api/signup', {
     method: 'post',
     body: JSON.stringify(body),
     headers: {
@@ -21,16 +20,77 @@ export const signUpUser = createAsyncThunk('signup', async (body) => {
   return json;
 });
 
+export const signIn = createAsyncThunk('user/signin', async (body) => {
+  const res = await fetch('/auth/api/signin', {
+    method: 'post',
+    body: JSON.stringify(body),
+    headers: {
+      'content-type': 'application/json'
+    }
+  });
+  const json = await res.json();
+  return json;
+});
+
+export const forgetUserName = createAsyncThunk(
+  'user/forgetusername',
+  async (body) => {
+    const res = await fetch('/api/auth/forget-username', {
+      method: 'post',
+      body: JSON.stringify(body),
+      headers: {
+        'content-type': 'application/json'
+      }
+    });
+    const json = await res.json();
+    return json;
+  }
+);
+
+export const forgetPassword = createAsyncThunk(
+  'user/forgetpassword',
+  async (body) => {
+    const res = await fetch('/api/auth/forget-password', {
+      method: 'post',
+      body: JSON.stringify(body),
+      headers: {
+        'content-type': 'application/json'
+      }
+    });
+    const json = await res.json();
+    return json;
+  }
+);
+
+export const changePassword = createAsyncThunk(
+  'user/changepassword',
+  async (body) => {
+    const res = await fetch('/api/auth/change-password', {
+      method: 'patch',
+      body: JSON.stringify(body),
+      headers: {
+        'content-type': 'application/json'
+      }
+    });
+    const json = await res.json();
+    return json;
+  }
+);
+
 const AuthSlice = createSlice({
   name: 'user',
   initialState: INITIAL_STATE,
   reducers: {},
   extraReducers: {
-    [signUpUser.fulfilled]: (state) => {
-      state.isLoading = true;
-    },
-    [signUpUser.rejected]: (state) => {
+    [signUpUser.fulfilled]: (state, action) => {
       state.isLoading = false;
+      state.user = action.payload;
+      state.isAuth = true;
+    },
+    [signUpUser.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.isAuth = false;
+      state.error = action.payload.error.message;
     },
     [signUpUser.pending]: (state) => {
       state.isLoading = true;
