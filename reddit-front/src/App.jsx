f1fb12410234665dba36c1d74190e815807177f8
ahmedasad236 <ahmedasad236@gmail.com>
2022-11-05 14:06:21 +0200

    origin/development
    - Modify the App.jsx file

--- reddit-front/src/App.jsx
+++ reddit-front/src/App.jsx
@@ -1,20 +1,5 @@
-import { ThemeProvider } from '@mui/material';
-import { StyledBody } from './components/GlobalStyles/GlobalStyles.style';
-// import Login from './pages/Login/Login';
-import SignUp from './pages/SignUp/SignUp';
-// import ForgetUserName from './pages/ForgetUserName/ForgetUserName';
-// import ForgetPasswordName from './pages/ForgetUserPassword/ForgetUserPassword';
-// import ResetPassword from './pages/ResetPassword/ResetPassword';
-import theme from './utilities/theme';
-
 function App() {
-  return (
-    <StyledBody>
-      <ThemeProvider theme={theme}>
-        <ResetPassword />
-      </ThemeProvider>
-    </StyledBody>
-  );
+  return <div>Hello</div>;
 }
 
 export default App;
