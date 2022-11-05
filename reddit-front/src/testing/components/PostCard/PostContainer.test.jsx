168de256b568abb18e9234defce74900ad72413d
AyaHusein2001 <aya.ahmed2001.aa@gmail.com>
2022-11-05 10:59:59 +0200

    feature/36/create_home_page_cards created all cards

---
+++ reddit-front/src/testing/components/PostCard/PostContainer.test.jsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import PostContainer from '../../../components/PostCard/PostContainer/PostContainer';
+
+// Checking for the rendering of the container component
+test('renders the post container', () => {
+  render(<PostContainer />);
+  const linkElement = screen.getByTestId(/test-post-container/i);
+  expect(linkElement).toBeInTheDocument();
+});
