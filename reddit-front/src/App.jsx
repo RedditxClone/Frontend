1782555b3fa1ad9ac6663f2292b85e33b81cf920
AyaHusein2001 <aya.ahmed2001.aa@gmail.com>
2022-11-06 21:04:08 +0200

    Edited Home Communities Card to make it reusable in other pages and edited some cards styles as well

--- reddit-front/src/App.jsx
+++ reddit-front/src/App.jsx
@@ -1,5 +1,59 @@
+/* eslint-disable */
+import HomeCommunitiesCard from "./components/HomePageCards/HomeCommunitiesCard";
+import pic from "./assets/Images/1166721.jpg";
+import HomeCreatePostCard from "./components/HomePageCards/HomeCreatePostCard";
+const communities = [
+  {
+    name: "My Community",
+    picture: pic,
+    growing: true,
+    goingDown: false,
+    rank: 1
+  },
+  {
+    name: "My Community",
+    picture: pic,
+    growing: false,
+    goingDown: true,
+    rank: 1
+  },
+  {
+    name: "My Community",
+    picture: pic,
+    growing: false,
+    goingDown: true,
+    rank: 1
+  },
+  {
+    name: "My Community",
+    picture: pic,
+    growing: true,
+    goingDown: false,
+    rank: 1
+  },
+  {
+    name: "My Community",
+    picture: pic,
+    growing: false,
+    goingDown: false,
+    rank: 1
+  }
+];
+
+const buttons1 = ["Top", "Gaming", "Near You"];
+const buttonText = "See All Health&Fit";
 function App() {
-  return <div>Hello</div>;
+  // return <HomeCreatePostCard />
+  return (
+    <HomeCommunitiesCard
+      buttons1={buttons1}
+      buttons2={buttons1}
+      pic={pic}
+      communities={communities}
+      homePageCard={false}
+      buttonText={buttonText}
+    />
+  );
 }
 
 export default App;
