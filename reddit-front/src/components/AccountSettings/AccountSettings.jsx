fcbcec9c180898a10f5af282cf37c436747b8bf7
Karim-Mohamed20 <karim.moh2052@gmail.com>
2022-11-10 13:55:50 +0200

    I have finished UI for account setting component feature/19/account_settings_web_eddition_ui

---
+++ reddit-front/src/components/AccountSettings/AccountSettings.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable comma-dangle */
+// import { Container } from '@mui/material';
+// import { makeStyles } from '@mui/styles';
+import AccountPreference from './AccountPreference/AccountPreference';
+import ConnectedAccounts from './ConnectedAccounts/ConnectedAccounts';
+import BetaTests from './BetaTests/BetaTests';
+import DeleteAccount from './DeleteAccount/DeleteAccount';
+import './AccountSettingsStyle.css';
+
+export default function AccountSettings() {
+  return (
+    <div className="account-settings">
+      <h2 className="h2">Account settings</h2>
+      <AccountPreference />
+      <ConnectedAccounts />
+      <BetaTests />
+      <DeleteAccount />
+    </div>
+  );
+}
---
+++ reddit-front/src/components/AccountSettings/DeleteAccount/DeleteAccount.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable object-curly-newline */
+/* eslint-disable comma-dangle */
+
+import { Button, Box } from '@mui/material';
+// import DeleteSharpIcon from '@mui/icons-material/DeleteSharp';
+import { RiDeleteBin5Fill } from 'react-icons/ri';
+import './DeleteAccount.css';
+import { useState } from 'react';
+import Box4 from '../ActionComponents.jsx/Box4';
+import Box5 from '../ActionComponents.jsx/Box5';
+
+export default function DeleteAccount() {
+  const [isDelete, setIsDelete] = useState(false);
+  const handleClickDelete = () => {
+    setIsDelete(true);
+  };
+  const [isShownDelContinue, setIsShownDelContinue] = useState(false);
+  return (
+    <>
+      {(isDelete || isShownDelContinue) && <div className="overlay"></div>}
+      <div className="delete-account">
+        <h3 className="main-h3">Delete Account</h3>
+        <Box className="del">
+          <Button
+            onClick={handleClickDelete}
+            startIcon={<RiDeleteBin5Fill style={{ color: '#ff585b' }} />}
+            variant="text"
+            style={{ color: '#ff585b' }}
+          >
+            Delete Account
+          </Button>
+          {isDelete && (
+            <Box4
+              setIsDelete={setIsDelete}
+              setIsShownDelContinue={setIsShownDelContinue}
+            />
+          )}
+          {isShownDelContinue && (
+            <Box5 setIsShownDelContinue={setIsShownDelContinue} />
+          )}
+        </Box>
+      </div>
+    </>
+  );
+}
