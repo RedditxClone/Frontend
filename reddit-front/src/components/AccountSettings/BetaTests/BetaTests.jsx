4f30d0654538498466f227bf9106d6b91ee0583c
Karim-Mohamed20 <karim.moh2052@gmail.com>
2022-11-10 17:14:29 +0200

    I have finished UI for account settings

---
+++ reddit-front/src/components/AccountSettings/BetaTests/BetaTests.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/self-closing-comp */
+import { Switch, Box } from '@mui/material';
+import './BetaTestsStyle.css';
+import { useState } from 'react';
+import Box3 from '../ActionComponents.jsx/Box3';
+
+export default function BetaTests() {
+  const [isToggled, setIsToggled] = useState(false);
+  const handleToggle = () => {
+    setIsToggled(true);
+  };
+  return (
+    <>
+      {isToggled && <div className="overlay"></div>}
+      <div className="beta-tests">
+        <h3 className="main-h3">Beta Tests</h3>
+        <div className="user-info">
+          <div className="user">
+            <h3 className="h3">Opt into beta tests</h3>
+            <p className="p">
+              See the newest features from Reddit and join the r/beta community
+            </p>
+          </div>
+          <Box className="cont">
+            <Switch />
+          </Box>
+        </div>
+        <div className="beta-2">
+          <div className="user">
+            <h3 className="h3">Opt out of the redesign</h3>
+            <p className="p">Revert back to old Reddit for the time being</p>
+          </div>
+          <Box className="cont">
+            <Switch onChange={handleToggle} />
+          </Box>
+          {isToggled && <Box3 setIsToggled={setIsToggled} />}
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ reddit-front/src/components/AccountSettings/ConnectedAccounts/ConnectedAccounts.jsx
@@ -0,0 +1,82 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/jsx-wrap-multilines */
+import './ConnectedAccountsStyle.css';
+import { Switch, Button, Box } from '@mui/material';
+import FacebookIcon from '@mui/icons-material/Facebook';
+// import GoogleIcon from '@mui/icons-material/Google';
+import google from '../../imgs/google.png';
+
+export default function ConnectedAccounts() {
+  return (
+    <div className="connected-accounts">
+      <h3 className="main-h3">Connected Accounts</h3>
+      <div className="account">
+        <div className="facebook">
+          <h3
+            className="h3"
+            id="fa-h3"
+          >
+            Connect to Facebook
+          </h3>
+          <p className="p">
+            Connect a Facebook account to enable the choice to share your new
+            posts and display a link on your profile. We will never post to
+            Facebook without your permission.
+          </p>
+          <Box className="cont">
+            <Button
+              startIcon={<FacebookIcon />}
+              variant="contained"
+              style={{ backgroundColor: '#1da1f2' }}
+            >
+              Connect to Facebook
+            </Button>
+          </Box>
+        </div>
+      </div>
+      <div className="disabled-div">
+        <div className="user">
+          <h3 className="h3">Show link on profile</h3>
+          <p className="p">
+            you can show a link to your Facebook account on your profile
+          </p>
+        </div>
+        <Box className="cont">
+          <Switch />
+        </Box>
+      </div>
+      <div className="account">
+        <div className="google">
+          <h3 className="h3">Connected to Google</h3>
+          <p className="p">Connect account to log in to Reddit with Google</p>
+        </div>
+        <Box className="cont">
+          <Button
+            // startIcon={
+            //   <GoogleIcon
+            //     style={{ color: '#1c1c1c', backgroundColor: '#ffffff' }}
+            //   />
+            // }
+            startIcon={
+              <img
+                src={google}
+                alt="google"
+                style={{ backgroundColor: '#ffffff' }}
+              />
+            }
+            variant="contained"
+            style={{ backgroundColor: '#1da1f2' }}
+          >
+            Connect to Google
+          </Button>
+          {/* <Button
+            style={{ color: '#1da1f2' }}
+            variant="text"
+          >
+            (disconnect)
+          </Button> */}
+        </Box>
+      </div>
+    </div>
+  );
+}
