7fd9c7fc51005d9e4f86b25286e60eebeb0e948c
ahmedasad236 <ahmedasad236@gmail.com>
2022-11-23 00:08:16 +0200

    feature/26/create_post
    - Add the title input
    - Add the text Editor for the post with its style

--- reddit-front/src/pages/CreatePost/ChooseCommunityName.jsx
+++ reddit-front/src/pages/CreatePost/ChooseCommunityName.jsx
@@ -1,19 +1,87 @@
+import { useRef, useState } from 'react';
+import { IoIosArrowDown } from 'react-icons/io';
 import classes from './CreatePost.module.css';
 
+const communities = [
+  { logo: 'logo1', name: 'comm1', members: '12356' },
+  { logo: 'logo2', name: 'comm2', members: '156' }
+];
+
 function ChooseCommunityName() {
+  const inputRef = useRef();
+  const [openList, setOpenList] = useState(false);
+  const onBlurInputHandler = () => {
+    setOpenList(false);
+  };
   return (
     <>
-      <div>
-        <span className={classes['community-icon']}>icon</span>
-        <div>
-          <input
-            className={classes['choose-community_input']}
-            placeholder="Choose Community"
-          />
-        </div>
-        <span className={classes['select-button']}>button</span>
+      <div className={classes['choose-community_container']}>
+        <span className={classes['choose-community_icon']} />
+        <input
+          ref={inputRef}
+          onBlur={onBlurInputHandler}
+          onFocus={() => setOpenList(true)}
+          className={classes['choose-community_input']}
+          placeholder="Choose Community"
+        />
+        <span
+          onClick={() => setOpenList((prev) => !prev)}
+          className={classes['select-button']}
+        >
+          <IoIosArrowDown />
+        </span>
       </div>
-      <div className={classes['communities-list']}> </div>
+      {openList ? (
+        <div className={classes['communities-list']}>
+          <div>
+            <span className={classes['section-label']}>Your profile</span>
+            <button
+              type="button"
+              className={classes.communities_item}
+            >
+              <span className={classes['community-logo']}>
+                <img
+                  src="#"
+                  alt=""
+                />
+              </span>
+              <div className={classes['community-info']}>
+                <span>u/usernmae</span>
+              </div>
+            </button>
+          </div>
+          <div>
+            <span className={classes['section-label']}>
+              Your communities
+              <button
+                type="button"
+                className={classes['create-community_btn']}
+              >
+                Create New
+              </button>
+            </span>
+            {communities.map((comm) => (
+              <button
+                type="button"
+                className={classes.communities_item}
+              >
+                <span className={classes['community-logo']}>
+                  <img
+                    src={comm.logo}
+                    alt={comm.name}
+                  />
+                </span>
+                <div className={classes['community-info']}>
+                  <span>{comm.name}</span>
+                  <span className={classes['community-members']}>
+                    {`${comm.members} members`}
+                  </span>
+                </div>
+              </button>
+            ))}
+          </div>
+        </div>
+      ) : null}
     </>
   );
 }
---
+++ reddit-front/src/pages/CreatePost/CreatePostEditor.jsx
@@ -0,0 +1,106 @@
+/* eslint-disable object-curly-newline */
+import { useRef, useState } from 'react';
+import Draft from 'draft-js';
+import './CreatePost.module.css';
+import BlockStyleControls from './BlockStyleControls';
+import InlineStyleControls from './InlineStyleControls';
+import './CreatePostEditor.css';
+
+const { Editor, EditorState, RichUtils, getDefaultKeyBinding } = Draft;
+
+// Custom overrides for "code" style.
+const styleMap = {
+  CODE: {
+    backgroundColor: 'rgba(0, 0, 0, 0.05)',
+    fontFamily: '"Inconsolata", "Menlo", "Consolas", monospace',
+    fontSize: 16,
+    padding: 2
+  }
+};
+
+function CreatePostEditor() {
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  const editorRef = useRef();
+
+  const getBlockStyle = (block) => {
+    switch (block.getType()) {
+      case 'blockquote':
+        return 'RichEditor-blockquote';
+      default:
+        return null;
+    }
+  };
+  const focus = () => {
+    editorRef.focus();
+  };
+  const onChangeHandler = (_editorState) => {
+    setEditorState(_editorState);
+  };
+
+  const handleKeyCommand = (command, _editorState) => {
+    const newState = RichUtils.handleKeyCommand(_editorState, command);
+    if (newState) {
+      onChangeHandler(newState);
+      return true;
+    }
+    return false;
+  };
+
+  const mapKeyToEditorCommand = (e) => {
+    if (e.keyCode === 9 /* TAB */) {
+      const newEditorState = RichUtils.onTab(e, editorState, 4 /* maxDepth */);
+      if (newEditorState !== editorState) {
+        onChangeHandler(newEditorState);
+      }
+    }
+    return getDefaultKeyBinding(e);
+  };
+
+  const toggleBlockType = (blockType) => {
+    onChangeHandler(RichUtils.toggleBlockType(editorState, blockType));
+  };
+  const toggleInlineStyle = (inlineStyle) => {
+    onChangeHandler(RichUtils.toggleInlineStyle(editorState, inlineStyle));
+  };
+
+  let className = 'RichEditor-editor';
+  const contentState = editorState.getCurrentContent();
+  if (!contentState.hasText()) {
+    if (contentState.getBlockMap().first().getType() !== 'unstyled') {
+      className += ' RichEditor-hidePlaceholder';
+    }
+  }
+
+  return (
+    <div className="RichEditor-root">
+      <div className="RichEditor-controls">
+        <InlineStyleControls
+          editorState={editorState}
+          onToggle={toggleInlineStyle}
+        />
+        <BlockStyleControls
+          editorState={editorState}
+          onToggle={toggleBlockType}
+        />
+      </div>
+      <div
+        className={className}
+        onClick={focus}
+      >
+        <Editor
+          blockStyleFn={getBlockStyle}
+          customStyleMap={styleMap}
+          editorState={editorState}
+          handleKeyCommand={handleKeyCommand}
+          keyBindingFn={mapKeyToEditorCommand}
+          onChange={onChangeHandler}
+          placeholder="Text(optional)"
+          ref={editorRef}
+          spellCheck
+        />
+      </div>
+    </div>
+  );
+}
+
+export default CreatePostEditor;
