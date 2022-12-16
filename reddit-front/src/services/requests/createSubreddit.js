a6428417b6fce459a3134ad68e7e1ebcee737edd
ahmedasad236 <ahmedasad236@gmail.com>
2022-12-16 20:00:40 +0200

    api/165/get_communities_for_user
    - Create a slice to save communities of the user
    - Add the token in headers of the createCommunity request

--- reddit-front/src/services/requests/createSubreddit.js
+++ reddit-front/src/services/requests/createSubreddit.js
@@ -1,12 +1,14 @@
 import api from './api';
+import getCookie from './getCookie';
 
+const token = getCookie('Authorization');
 const createSubreddit = async (communityName, communityType, isOver18) => {
   try {
     const response = await api.post('/api/subreddit/', {
       name: communityName,
       type: communityType,
       over18: isOver18
-    });
+    }, { headers: { Authorization: token } });
     if (response.status >= 200 && response.status < 300) {
       console.log(response.data);
       return true;
